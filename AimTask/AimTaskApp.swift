5df0bf11bfbae2e490e0e7706a7e63e005d8c534
vilayath01 <vilayathussain290@gmail.com>
2024-07-20 12:23:20 +1000

    Initial Commit

---
+++ AimTask/AimTaskApp.swift
@@ -0,0 +1,17 @@
+//
+//  AimTaskApp.swift
+//  AimTask
+//
+//  Created by Vilayath Mohammed on 20/7/2024.
+//
+
+import SwiftUI
+
+@main
+struct AimTaskApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
