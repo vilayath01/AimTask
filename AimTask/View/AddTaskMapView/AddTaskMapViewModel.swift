736ac0639b50ecc8262c435145bda5526c426885
vilayath01 <vilayath01@gmail.com>
2024-08-26 22:15:22 +1000

    [AIM-013] Geofence work 1 added  location GPX file to test geofence

--- AimTask/View/AddTaskMapView/AddTaskMapViewModel.swift
+++ AimTask/View/AddTaskMapView/AddTaskMapViewModel.swift
@@ -11,7 +11,7 @@ import Combine
 import SwiftUI
 
 class AddTaskMapViewModel: NSObject, ObservableObject {
-    
+    //Map related properties
     @Published var searchText: String = ""
     @Published var region: MKCoordinateRegion = MKCoordinateRegion(
         center: CLLocationCoordinate2D(latitude: 0.0, longitude: 0.0),
@@ -21,31 +21,75 @@ class AddTaskMapViewModel: NSObject, ObservableObject {
     @Published var errorMessage: String?
     @Published var addressName: String = ""
     
+    // Geofence-related properties
+    @Published var tasks: [TaskModel] = []
+    private var geofenceRegions: [String: CLCircularRegion] = [:]
+    
+    //other properties
     private var geocoder = CLGeocoder()
     private var completer = MKLocalSearchCompleter()
     private var cancellables = Set<AnyCancellable>()
     private var locationManager = CLLocationManager()
+    private var fdbManager: FDBManager
     
-    override init() {
+    private var previousLocation: CLLocation?
+    
+    init(fdbManager: FDBManager = FDBManager()) {
+        self.fdbManager = fdbManager
         super.init()
+        fetchTasks()
         setupBindings()
         completer.resultTypes = .address
         completer.delegate = self
+        setupLocationManager()
+    }
+    
+    private func setupLocationManager() {
         locationManager.delegate = self
-        requestLocationPermission()
-        //        locationManager.allowsBackgroundLocationUpdates = true
-        //        locationManager.pausesLocationUpdatesAutomatically = false
-        
+        locationManager.requestWhenInUseAuthorization()
+        locationManager.requestAlwaysAuthorization()
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest
+        locationManager.startUpdatingLocation()
+        locationManager.allowsBackgroundLocationUpdates = true
+        locationManager.pausesLocationUpdatesAutomatically = false
     }
     
     private func setupBindings() {
         $searchText
             .debounce(for: .seconds(0.5), scheduler: DispatchQueue.main)
             .removeDuplicates()
+            .receive(on: DispatchQueue.main) // Ensure updates happen on the main thread
             .sink { [weak self] text in
                 self?.completer.queryFragment = text
             }
             .store(in: &cancellables)
+    }
+    
+    func zoomInOption() {
+        DispatchQueue.main.async {
+            self.region.span.latitudeDelta /= 2.0
+            self.region.span.longitudeDelta /= 2.0
+        }
+    }
+    
+    func zoomOutOption() {
+        // Zoom out action
+        let maxSpan: CLLocationDegrees = 180.0
+        
+        var newLatDalta = region.span.latitudeDelta * 2
+        var newlongDelta = region.span.longitudeDelta * 2
+        
+        if newLatDalta > maxSpan {
+            newLatDalta = maxSpan
+        }
+        
+        if newlongDelta > maxSpan {
+            newlongDelta = maxSpan
+        }
+        DispatchQueue.main.async {
+            self.region.span = MKCoordinateSpan(latitudeDelta: newLatDalta, longitudeDelta: newlongDelta)
+        }
+        
     }
     
     func updateSearchText(_ text: String) {
@@ -67,9 +111,11 @@ class AddTaskMapViewModel: NSObject, ObservableObject {
             
             if let placemark = placemarks?.first,
                let location = placemark.location {
+                
                 DispatchQueue.main.async {
                     self?.region = MKCoordinateRegion(center: location.coordinate, span: MKCoordinateSpan(latitudeDelta: 0.05, longitudeDelta: 0.05))
                     self?.errorMessage = nil
+                    self?.addressName = "\(placemark.name ?? ""), \(placemark.locality ?? ""), \(placemark.country ?? "")."
                 }
             } else {
                 DispatchQueue.main.async {
@@ -105,27 +151,22 @@ class AddTaskMapViewModel: NSObject, ObservableObject {
         }
     }
     
-    func requestLocationPermission() {
-        guard CLLocationManager.locationServicesEnabled() else {
-            DispatchQueue.main.async {
-                self.errorMessage = "Location services are not enabled."
-            }
-            return
+    // Map Recenter
+    func centerOnUserLocation() {
+        if let location = locationManager.location {
+            centerMap(on: location.coordinate, animated: true)
+            reverseGeocodeLocation(location)
         }
-        
-        switch locationManager.authorizationStatus {
-        case .notDetermined:
-            locationManager.requestWhenInUseAuthorization()
-        case .restricted, .denied:
-            DispatchQueue.main.async {
-                self.errorMessage = "Location access is restricted or denied."
+    }
+    
+    func centerMap(on coordinate: CLLocationCoordinate2D, animated: Bool) {
+        if animated {
+            withAnimation(.easeInOut(duration: 1.0)) {
+                region.center = coordinate
+                region.span = MKCoordinateSpan(latitudeDelta: 0.05, longitudeDelta: 0.05)
             }
-        case .authorizedWhenInUse, .authorizedAlways:
-            locationManager.requestLocation()
-        @unknown default:
-            DispatchQueue.main.async {
-                self.errorMessage = "Unknown authorization status."
-            }
+        } else {
+            region.center = coordinate
         }
     }
 }
@@ -146,33 +187,35 @@ extension AddTaskMapViewModel: MKLocalSearchCompleterDelegate {
 
 extension AddTaskMapViewModel: CLLocationManagerDelegate {
     func locationManager(_ manager: CLLocationManager, didChangeAuthorization authorizationStatus: CLAuthorizationStatus) {
-        if authorizationStatus == .authorizedWhenInUse || authorizationStatus == .authorizedAlways {
+        switch authorizationStatus {
+        case .authorizedWhenInUse, .authorizedAlways:
             locationManager.requestLocation()
-        } else if authorizationStatus == .denied || authorizationStatus == .restricted {
+        case .denied, .restricted:
             DispatchQueue.main.async {
                 self.errorMessage = "Location access is restricted or denied."
             }
+        default:
+            break
         }
     }
     
     func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
-        if let location = locations.first {
-            geocoder.reverseGeocodeLocation(location) { [weak self] placemarks, error in
-                DispatchQueue.main.async {
-                    guard let placemark = placemarks?.first else {
-                        return
-                    }
-                    
-                    self?.addressName = "\(placemark.name ?? ""), \(placemark.locality ?? ""), \(placemark.country ?? "")."
-                }
-                
-            }
-            DispatchQueue.main.async {
-                withAnimation(.easeIn(duration: 1.0)) {
-                    self.region = MKCoordinateRegion(center: location.coordinate, span: MKCoordinateSpan(latitudeDelta: 0.05, longitudeDelta: 0.05))
-                }
+        guard let location = locations.first else { return }
+        
+        // Compare with the previous location
+        if let previousLocation = previousLocation {
+            let distance = location.distance(from: previousLocation)
+            if distance < 10 {  // Only update if the location has changed by more than 10 meters
+                return
             }
         }
+        
+        // Update the previousLocation to the current one
+        previousLocation = location
+        
+        // Perform reverse geocoding
+        reverseGeocodeLocation(location)
+        
     }
     
     func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
@@ -181,18 +224,13 @@ extension AddTaskMapViewModel: CLLocationManagerDelegate {
         }
     }
     
-    // Geofence
-    
-    func startMonitoring(geofenceRegion: CLCircularRegion) {
-        if CLLocationManager.isMonitoringAvailable(for: CLCircularRegion.self) {
-            locationManager.startMonitoring(for: geofenceRegion)
-        } else {
-            print("Geofenceing is not supported on this device.")
+    private func reverseGeocodeLocation(_ location: CLLocation) {
+        geocoder.reverseGeocodeLocation(location) { [weak self] placemarks, error in
+            DispatchQueue.main.async {
+                guard let placemark = placemarks?.first else { return }
+                self?.addressName = "\(placemark.name ?? ""), \(placemark.locality ?? ""), \(placemark.country ?? "")."
+            }
         }
-    }
-    
-    func stopMonitoring(geofenceRegion: CLCircularRegion) {
-        locationManager.stopMonitoring(for: geofenceRegion)
     }
 }
 
@@ -202,7 +240,71 @@ extension CLLocationCoordinate2D: Identifiable {
     }
 }
 
+
+// Geofence
 extension AddTaskMapViewModel {
+    
+    func fetchTasks() {
+        fdbManager.fetchTasks()
+        fdbManager.$tasks
+            .sink { [weak self] tasks in
+                self?.tasks = tasks
+                self?.updateGeofences()
+            }
+            .store(in: &cancellables)
+    }
+    
+    
+    private func updateGeofences() {
+        clearGeofences()
+        
+        // Add new geofences for each task
+        for task in tasks {
+            addGeofence(for: task)
+        }
+        
+        print("Updated geofences: \(geofenceRegions.keys)")
+    }
+    
+    func clearGeofences() {
+        for region in geofenceRegions.values {
+            stopMonitoring(geofenceRegion: region)
+            print("Stopped monitoring for geofence: \(region.identifier)")
+        }
+        geofenceRegions.removeAll()
+        print("Cleared all geofences")
+    }
+    
+    func addGeofence(for task: TaskModel) {
+        let geofenceRegion = CLCircularRegion(
+            center: task.coordinate,
+            radius: 100.0, // Define the radius for the geofence
+            identifier: task.documentID
+        )
+        
+        geofenceRegion.notifyOnEntry = true
+        geofenceRegion.notifyOnExit = true
+        
+        // Start monitoring the geofence region
+        startMonitoring(geofenceRegion: geofenceRegion)
+        
+        // Store the region for future reference
+        geofenceRegions[task.documentID] = geofenceRegion
+        print("Started monitoring geofence for task: \(task.documentID) / \(task.locationName)")
+    }
+    
+    func startMonitoring(geofenceRegion: CLCircularRegion) {
+        if CLLocationManager.isMonitoringAvailable(for: CLCircularRegion.self) {
+            locationManager.startMonitoring(for: geofenceRegion)
+            print("This is monitor region: \(geofenceRegion.identifier)/ \(geofenceRegion.radius)")
+        } else {
+            print("Geofenceing is not supported on this device.")
+        }
+    }
+    
+    func stopMonitoring(geofenceRegion: CLCircularRegion) {
+        locationManager.stopMonitoring(for: geofenceRegion)
+    }
     func locationManager(_ manager: CLLocationManager, didEnterRegion region: CLRegion) {
         if region is CLCircularRegion {
             print("Entered geofence: \(region.identifier)")
@@ -212,7 +314,7 @@ extension AddTaskMapViewModel {
     
     func locationManager(_ manager: CLLocationManager, didExitRegion region: CLRegion) {
         if region is CLCircularRegion {
-            print("Exited geofence: \(region.identifier)")
+            print("Exited geofence: \(String(describing: manager.location))")
             // Handle exit event
         }
     }
